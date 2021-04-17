60833794529355ba2fc2b4c2bced6f5bbcc18d28
Barvent <67266826+Barvent@users.noreply.github.com>
2021-04-17 19:38:59 +0200

    Added endpoint to receive the rule and the request

--- authorization-service/src/main/java/mpegg/authorization/authorizationservice/Controller/AuthorizationController.java
+++ authorization-service/src/main/java/mpegg/authorization/authorizationservice/Controller/AuthorizationController.java
@@ -1,21 +1,26 @@
 package mpegg.authorization.authorizationservice.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import mpegg.authorization.authorizationservice.Utils.FileUtil;
+import org.springframework.web.bind.annotation.*;
 import org.wso2.balana.Balana;
 import org.wso2.balana.PDP;
+import org.wso2.balana.PDPConfig;
+import org.wso2.balana.finder.PolicyFinder;
+import org.wso2.balana.finder.PolicyFinderModule;
 import org.wso2.balana.finder.impl.FileBasedPolicyFinderModule;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
+import java.util.Set;
 
 @RestController
 public class AuthorizationController {
     @PostMapping("/authorize")
-    String authorize(@RequestBody String request) {
+    String authorize(@RequestParam String request) {
         try {
             String policyLocation = (new File("src")).getCanonicalPath() + File.separator + "main" + File.separator + "resources" + File.separator + "support" + File.separator + "policy";
             System.setProperty(FileBasedPolicyFinderModule.POLICY_DIR_PROPERTY, policyLocation);
@@ -25,5 +30,52 @@ public class AuthorizationController {
         Balana balana = Balana.getInstance();
         PDP pdp = new PDP(balana.getPdpConfig());
         return pdp.evaluate(request);
+    }
+    @PostMapping("/authorize_rule")
+    String authorize_rule(@RequestParam String request, @RequestParam String rule, HttpServletResponse response) {
+        FileUtil fu = new FileUtil();
+        String policyLocation = null;
+        String name = null;
+        try {
+            name = fu.randomFileName();
+            policyLocation = (new File("src")).getCanonicalPath() + File.separator + "main" + File.separator + "resources" + File.separator + "support" + File.separator + "policy";
+            policyLocation += File.separator + name;
+            fu.createDirectory(policyLocation);
+            fu.createFile(policyLocation + File.separator + "policy.xml", rule);
+        } catch (IOException e) {
+            e.printStackTrace();
+            response.setStatus(503);
+            return "Server error";
+        }
+        String result = null;
+        try {
+            PolicyFinder policyFinder = new PolicyFinder();
+            Set<PolicyFinderModule> modules = new HashSet<PolicyFinderModule>();
+            Set<String> policySet = new HashSet<String>();
+            policySet.add(policyLocation);
+            modules.add(new FileBasedPolicyFinderModule(policySet));
+            policyFinder.setModules(modules);
+            PDPConfig pdpc = new PDPConfig(null, policyFinder, null);
+            PDP pdp = new PDP(pdpc);
+            result = pdp.evaluate(request);
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            try {
+                fu.deleteDirectory(policyLocation);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            response.setStatus(503);
+            return "Server error";
+        }
+        try {
+            fu.deleteDirectory(policyLocation);
+        } catch (IOException e) {
+            e.printStackTrace();
+            response.setStatus(503);
+            return "Server error";
+        }
+        return result;
     }
 }
